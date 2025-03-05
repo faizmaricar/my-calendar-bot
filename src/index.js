0d004307f6ca013a6501f32752fb9d24f735705d
Faiz Maricar <maricar.faiz@gmail.com>
2025-03-05 13:06:19 +0800

    log today

--- src/index.js
+++ src/index.js
@@ -10,17 +10,18 @@ const today = format(now, "yyyy-MM-dd");
 
 getPrayerTimes(today).then((res) => {
   const { hijri_date, ...prayers } = res;
-  for (let prayer in prayers) {
-    const time = prayers[prayer];
+  console.log(today);
+  //  for (let prayer in prayers) {
+  //    const time = prayers[prayer];
 
-    const event = createEvent(
-      prayer.charAt(0).toUpperCase() + prayer.slice(1),
-      today,
-      time
-    );
+  //    const event = createEvent(
+  //      prayer.charAt(0).toUpperCase() + prayer.slice(1),
+  //      today,
+  //      time
+  //    );
 
-    addEventToGoogleCalendar(event)
-      .then((response) => console.log("Event created:", response.data.htmlLink))
-      .catch((error) => console.error("Error creating event:", error));
-  }
+  //    addEventToGoogleCalendar(event)
+  //      .then((response) => console.log("Event created:", response.data.htmlLink))
+  //      .catch((error) => console.error("Error creating event:", error));
+  //  }
 });
