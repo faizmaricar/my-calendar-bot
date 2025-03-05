c60e0241316681b6b096adba9c743656e8f73963
Faiz Maricar <maricar.faiz@gmail.com>
2025-03-05 13:14:05 +0800

    remove parse

--- src/getStartAndEndTime.js
+++ src/getStartAndEndTime.js
@@ -5,8 +5,8 @@ function getStartAndEndTime(prayerDate, time) {
   const date = new Date();
   const datetimestring = `${prayerDate} ${time}`;
 
-  const startTime = formatRFC3339(parse(datetimestring, dateFormat, date));
-  const endTime = formatRFC3339(addMinutes(startTime, 5));
+  const startTime = parse(datetimestring, dateFormat, date);
+  const endTime = addMinutes(startTime, 5);
 
   return [startTime, endTime];
 }
